42f3d28538349bee6d500675ef7febc00bfe8bde
HappyPC\Happy <recr.happy156@gmail.com>
2016-10-31 01:55:06 +0900

    ティーポットの表示、freeglutのテストビルド

--- OpenGL-Billiards/OpenGL-Billiards/main.cpp
+++ OpenGL-Billiards/OpenGL-Billiards/main.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-int main() {
-	cout << "Hello world" << endl;
+#include <GL/freeglut.h>
+#pragma comment(lib, "freeglut.lib")
+
+void display()
+{
+	glClearColor(0.f, 0.f, 0.f, 1.f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glutSolidTeapot(0.5);
+
+	glutSwapBuffers();
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutCreateWindow(argv[0]);
+	glutDisplayFunc(display);
+	glutMainLoop();
+
 	return 0;
 }
